dd28b16059a01dbfde4b812e895f0b6745efbe1d
सिंकु कुमार <sudosinku@gmail.com>
2025-02-15 13:32:06 +0530

    Add initial Django app structure and environment variable support

--- KitabiKeeda/KitabiKeeda/settings.py
+++ KitabiKeeda/KitabiKeeda/settings.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Environment variables
+ENV_SECRET_KEY = os.getenv('SECRET_KEY')
+ENV_DEBUG = os.getenv('DEBUG')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yz#5g^kn==8i#(2*%*ig*m^yex)7ja&)guf1fx^53qsy3uu#h*'
+SECRET_KEY = ENV_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ENV_DEBUG
 
 ALLOWED_HOSTS = []
 
@@ -105,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
---
+++ KitabiKeeda/keeda/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KeedaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'keeda'
