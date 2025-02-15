89967992f6c8fdf67b91a16f9302c0c3d00d3f9d
सिंकु कुमार <73785795+SinkuKumar@users.noreply.github.com>
2025-02-15 15:50:36 +0530

    Add files via upload

--- KitabiKeeda/keeda/models.py
+++ KitabiKeeda/keeda/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Assistant(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    object = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    name = models.CharField(max_length=1024)
+    description = models.TextField(null=True)
+    model = models.CharField(max_length=1024)
+    instructions = models.TextField()
+    tools = models.JSONField()
+    metadata = models.JSONField()
+    top_p = models.FloatField()
+    temperature = models.FloatField()
+    response_format = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class VectorStore(models.Model):
-    pass
+    id = models.CharField(max_length=255, primary_key=True)
+    object = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    name = models.CharField(max_length=1024)
+    bytes = models.IntegerField()
+    file_counts = models.JSONField()
+
+    def __str__(self):
+        return self.name
